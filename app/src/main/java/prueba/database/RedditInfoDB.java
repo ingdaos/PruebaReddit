bcddc943658d3530441b5a0453cf9d463f59d685
DanielOchoa <ingdaos@gmail.com>
2017-01-23 22:57:43 -0500

    Información necesaria para almacenar modo offline

---
+++ app/src/main/java/prueba/database/RedditInfoDB.java
@@ -0,0 +1,55 @@
+package prueba.database;
+
+/**
+ * Created by PERSONAL on 23/01/2017.
+ */
+
+public class RedditInfoDB {
+
+    private String display_name;
+    private String display_icon;
+    private String info_description;
+    private String url_banner_img;
+    private String title;
+
+    public String getDisplay_name() {
+        return display_name;
+    }
+
+    public void setDisplay_name(String display_name) {
+        this.display_name = display_name;
+    }
+
+    public String getDisplay_icon() {
+        return display_icon;
+    }
+
+    public void setDisplay_icon(String display_icon) {
+        this.display_icon = display_icon;
+    }
+
+    public String getInfo_description() {
+        return info_description;
+    }
+
+    public void setInfo_description(String info_description) {
+        this.info_description = info_description;
+    }
+
+    public String getUrl_banner_img() {
+        return url_banner_img;
+    }
+
+    public void setUrl_banner_img(String url_banner_img) {
+        this.url_banner_img = url_banner_img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
